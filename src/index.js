ee88dc3d0fbb6e31f5d07b62c732addeb53762c1
Edik01 <128157224+Edik01@users.noreply.github.com>
2023-08-20 15:03:20 -0500

    summary

--- src/index.js
+++ src/index.js
@@ -1,29 +1,73 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import SimpleLightbox from 'simplelightbox';
 import { getData } from './api';
 import { createMarkup } from './markup';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a');
+
 let page = 1;
+
+let value = '';
 
 const formEl = document.querySelector('.search-form');
 
 const galleryEl = document.querySelector('.gallery');
+
+const btnEl = document.querySelector('.load-more');
+btnEl.addEventListener('click', onClick);
+async function onClick() {
+  try {
+    page += 1;
+    const { hits, totalHits } = await getData(value, page);
+    const markup = createMarkup(hits);
+
+    addMarkup(markup);
+    lightbox.refresh();
+    if (page * 40 >= totalHits) {
+      btnEl.classList.add('is-hidden');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    onError(error.message);
+  }
+}
 
 formEl.addEventListener('submit', onSubmit);
 
 async function onSubmit(event) {
   try {
     event.preventDefault();
-    const value = event.target.elements.searchQuery.value.trim();
+    value = event.target.elements.searchQuery.value.trim();
     if (!value) {
       return;
     }
+    page = 1;
     const { hits, totalHits } = await getData(value, page);
+    if (!hits.length) {
+      Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    if (hits.length < 40) {
+      btnEl.classList.add('is-hidden');
+    } else {
+      btnEl.classList.remove('is-hidden');
+    }
+
     const markup = createMarkup(hits);
     clear();
     addMarkup(markup);
-  } catch (error) {}
+
+    lightbox.refresh();
+  } catch (error) {
+    onError(error.message);
+  }
 }
 
 function addMarkup(markup) {
@@ -32,4 +76,8 @@ function addMarkup(markup) {
 
 function clear() {
   galleryEl.innerHTML = '';
+}
+
+function onError(message) {
+  Notify.failure(message);
 }
--- src/markup/index.js
+++ src/markup/index.js
@@ -10,27 +10,27 @@ export function createMarkup(items = []) {
         largeImageURl,
         tags,
       }) => {
-        return `<div class="photo-card">
+        return `<a href='${largeImageURl}' class="photo-card">
     <img src="${webformatURL}" alt="${tags}" loading="lazy" />
     <div class="info">
-      <p class="info-item">
+      <div class="info-item">
         <b>Likes</b>
         <p>${likes}</p>
-      </p>
-      <p class="info-item">
+      </div>
+      <div class="info-item">
         <b>Views</b>
         <p>${views}</p>
-      </p>
-      <p class="info-item">
+      </div>
+      <div class="info-item">
         <b>Comments</b>
     <p>${comments}</p>
-      </p>
-      <p class="info-item">
+      </div>
+      <div class="info-item">
         <b>Downloads</b>
         <p>${downloads}</p>
-      </p>
+      </div>
     </div>
-  </div>`;
+  </a>`;
       }
     )
     .join('');
